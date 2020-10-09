30d07e560dfdcab82271af02ca1c6a06a43e6758
Eileen1202 <42709916+Eileen1202@users.noreply.github.com>
2020-10-09 21:26:13 +0800

    v3

--- js/custom.js
+++ js/custom.js
@@ -25,13 +25,15 @@ var query_find = function(tableName, userlist, f = 1){
 			itoi = new Array();
 			exp = new Array();
 			var multiItemSlider = $('.slick-multiItemSlider');
-			var multiItem = $('.slick-multiItem');
+			var multiItem  = $('.slick-multiItem');
+			var multiItem1 = $('.slick-multiItem1');
 			var multiItem2 = $('.slick-multiItem2');
 			var multiItem3 = $('.slick-multiItem3');
         
             if(res.length >  0 ){
-				multiItemSlider.slick('removeSlide', null, null, true);
+				//multiItemSlider.slick('removeSlide', null, null, true);
 				multiItem.slick('removeSlide', null, null, true);
+				multiItem1.slick('removeSlide', null, null, true);
 				multiItem2.slick('removeSlide', null, null, true);
 				multiItem3.slick('removeSlide', null, null, true);
             }
@@ -105,6 +107,7 @@ var query_find = function(tableName, userlist, f = 1){
 			
 			multiItemSlider[0].slick.refresh();
 			multiItem[0].slick.refresh();
+			multiItem1[0].slick.refresh();
 			multiItem2[0].slick.refresh();
 			multiItem3[0].slick.refresh();
 			status.fadeOut(); // will first fade out the loading animation 
@@ -161,6 +164,7 @@ $(window).on('load', function() { // makes sure the whole site is loaded
 		//js for tabs
 		var tabsClick = $('.tabs .tab-links a, .tab-links-2 a, .tab-links-3 a');
 		var multiItem = $('.slick-multiItem');
+		var multiItem1 = $('.slick-multiItem1');
 		var multiItem2 = $('.slick-multiItem2');
 		var multiItem3 = $('.slick-multiItem3');
 		tabsClick.on('click', function(e)  {
@@ -173,6 +177,7 @@ $(window).on('load', function() { // makes sure the whole site is loaded
 			e.preventDefault();
 			//reset position for tabs
 			multiItem.slick('setPosition');
+			multiItem1.slick('setPosition');
 			multiItem2.slick('setPosition');
 			multiItem3.slick('setPosition');
 		});
@@ -218,6 +223,41 @@ $(window).on('load', function() { // makes sure the whole site is loaded
 		initializeClock('clockdiv', deadline);
 		
 		multiItem.slick({
+			infinite: true,
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			arrows: false,
+			draggable:true,
+			// autoplay: true,
+			// autoplaySpeed: 2000,
+			dots: true,
+			responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: true
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+				}
+			}
+			]
+		});
+		multiItem1.slick({
 			infinite: true,
 			slidesToShow: 4,
 			slidesToScroll: 4,
